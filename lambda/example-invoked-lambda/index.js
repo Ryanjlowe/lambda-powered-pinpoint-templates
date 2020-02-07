69b497329f0d9f243ae54a3f7704d3c4e30a56a3
Ryan Lowe <rjlowe@amazon.com>
2020-02-07 09:26:32 -0700

    Updating comments in example

--- lambda/example-invoked-lambda/index.js
+++ lambda/example-invoked-lambda/index.js
@@ -1,4 +1,6 @@
 exports.handler = async (event) => {
+    // Image this is a database call, or a webservice call, or anything!
+    // event.Endpoint would contain the full Endpoint object as stored in Pinpoint
     return {
         "image": "https://images.unsplash.com/photo-1580750603266-cae8b4b9f72a",
         "name": "Bananas",
